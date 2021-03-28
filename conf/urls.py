430bde257aa630255b0db455fb392d97cc1dba60
Alexander Ignatov <ignatov.alx@gmail.com>
2021-03-28 16:37:46 +0300

    change to re_path

--- conf/urls.py
+++ conf/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from vacancies.views import custom_500, MainView, VacanciesView, VacancyView, CompanyView
+from django.urls import re_path
 
 handler500 = custom_500
 
 urlpatterns = [
     path("", MainView.as_view(), name="main"),
-    path("vacancies/", VacanciesView.as_view(), name="vacancies"),
+    re_path(r"^vacancies/(?:cat/(?P<slug>\w+))?$", VacanciesView.as_view(), name="vacancies"),
     path("vacancies/<int:pk>", VacancyView.as_view(), name="vacancy"),
-    path("vacancies/cat/<str:slug>", VacanciesView.as_view(), name="vacancies_by_specialty"),
     path("companies/<int:pk>", CompanyView.as_view(), name="company"),
 ]
