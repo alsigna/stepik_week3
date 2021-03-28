8b676f6af100ae736b1ae9bde3aca6fed08cc2e5
Alexander Ignatov <ignatov.alx@gmail.com>
2021-03-28 15:38:37 +0300

    for check

--- conf/urls.py
+++ conf/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from vacancies.views import custom_500, MainView, VacanciesView, VacancyView, CompanyViewVer4
+from vacancies.views import custom_500, MainView, VacanciesView, VacancyView, CompanyView
 
 handler500 = custom_500
 
@@ -7,6 +7,6 @@ urlpatterns = [
     path("", MainView.as_view(), name="main"),
     path("vacancies/", VacanciesView.as_view(), name="vacancies"),
     path("vacancies/<int:pk>", VacancyView.as_view(), name="vacancy"),
-    path("vacancies/cat/<str:specialty_code>", VacanciesView.as_view(), name="vacancies_by_specialty"),
-    path("companies/<int:pk>", CompanyViewVer4.as_view(), name="company"),
+    path("vacancies/cat/<str:slug>", VacanciesView.as_view(), name="vacancies_by_specialty"),
+    path("companies/<int:pk>", CompanyView.as_view(), name="company"),
 ]
--- vacancies/views.py
+++ vacancies/views.py
@@ -1,16 +1,8 @@
 from django.db.models import Count
-from django.http import HttpResponseNotFound
 from django.views.generic import ListView, DetailView
 from django.views.generic.base import TemplateView
 from django.http import Http404
-from django.shortcuts import render
-from django.views.generic import View
 from .models import Company, Specialty, Vacancy
-from django.shortcuts import get_object_or_404
-
-
-def custom_404(request, exception):
-    return HttpResponseNotFound("Page Not Found")
 
 
 def custom_500(request):
@@ -32,7 +24,7 @@ class VacanciesView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        specialty = self.kwargs.get("specialty_code", None)
+        specialty = self.kwargs.get("slug", None)
         if specialty:
             context["title"] = Specialty.objects.get(code=specialty).title
         else:
@@ -42,7 +34,7 @@ class VacanciesView(ListView):
     def get_queryset(self):
         queryset = super().get_queryset()
 
-        specialty = self.kwargs.get("specialty_code", None)
+        specialty = self.kwargs.get("slug", None)
         if specialty:
             return queryset.filter(specialty__code=specialty)
         else:
@@ -52,47 +44,11 @@ class VacanciesView(ListView):
 class VacancyView(DetailView):
     model = Vacancy
 
-
-# via DetailView
-class CompanyViewVer1(DetailView):
-    model = Company
+    def get_queryset(self):
+        return super().get_queryset().select_related("company")
 
 
-# the same via View + get_object_or_404
-class CompanyViewVer2(View):
-    def get(self, request, *args, **kwargs):
-        company_pk = self.kwargs.get("pk", None)
-        if not company_pk:
-            raise Http404
-        company = get_object_or_404(Company, pk=company_pk)
-        return render(
-            request=request,
-            template_name="vacancies/company_detail.html",
-            context={
-                "object": company,
-            },
-        )
-
-
-# the same via View + prefetch_related
-class CompanyViewVer3(View):
-    def get(self, request, *args, **kwargs):
-        company_pk = self.kwargs.get("pk", None)
-        if not company_pk:
-            raise Http404
-        company = Company.objects.prefetch_related("vacancies").filter(pk=company_pk).first()
-        if not (company):
-            raise Http404
-        return render(
-            request=request,
-            template_name="vacancies/company_detail.html",
-            context={
-                "object": company,
-            },
-        )
-
-
-class CompanyViewVer4(DetailView):
+class CompanyView(DetailView):
     model = Company
 
     def get_queryset(self):
